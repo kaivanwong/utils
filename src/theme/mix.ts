b0a3d79f1427140c17e64e906c9333d1304a34b7
kaivanwong <kaivanwong@outlook.com>
2023-03-14 15:42:49 +0800

    add get color level method

--- src/theme/mix.ts
+++ src/theme/mix.ts
@@ -26,3 +26,34 @@ export const colorMix = (
   const _b = (`0${(b || 0).toString(16)}`).slice(-2)
   return `#${_r}${_g}${_b}`
 }
+
+/**
+ * @name getLevelColor
+ * @description Gets the level of a color
+ * @param color Hex Color, such as #ea3a72
+ * @param level Color Level, such as 0.1
+ * @param type Mix type, dark or light
+ * @returns
+ */
+export const mixColor = (color: string, level: number, type: string) => {
+  color = color.replace('#', '')
+  let red = parseInt(color.slice(0, 2), 16)
+  let green = parseInt(color.slice(2, 4), 16)
+  let blue = parseInt(color.slice(4, 6), 16)
+
+  if (type === 'light') {
+    if (level === 0)
+      return [red, green, blue].join(',')
+
+    red += Math.round(level * (255 - red))
+    green += Math.round(level * (255 - green))
+    blue += Math.round(level * (255 - blue))
+  }
+  else if (type === 'dark') {
+    red = Math.round((1 - level) * red)
+    green = Math.round((1 - level) * green)
+    blue = Math.round((1 - level) * blue)
+  }
+
+  return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
+}
